29b2d8f197334f0d0047fe6aa9af52b7379632b1
Fang <jmf133@dlh-m52.labs.pitt.edu>
2016-10-11 00:28:54 -0400

    Adds form, improves appearance

--- js/javascript.js
+++ js/javascript.js
@@ -1,52 +1,73 @@
-$("#greatForm").submit(function(e) {
-  e.preventDefault();
+$(document).ready(function(){
+
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 400, function(){
+
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+    });
+  });
+
+  $("#contactForm").submit(function(e) {
+    e.preventDefault();
+  });
 });
 
-function validate(){
-  var errorsNotPresent = 0;
+
+function validate() {
+
+  var radioErrorsNotPresent = 0;
   var errorsPresentInDoc = 0;
-  var liftRadios = document.getElementsByName('lift-status');
-  var cardioRadios = document.getElementsByName('cardio-status');
-  var problemAnswer = document.getElementById('problemStatus').value;
+  var nameV = document.getElementById('contactNameInput').value;
+  var emailV = document.getElementById('emailAddressInput').value;
+  var subjectV = document.getElementsByName('message-subject');
+  var messageV = document.getElementById('message').value;
   var errorArr = [];
 
-  for (var i=0; i<liftRadios.length; i++) {
-    if (liftRadios[i].checked) {
-      errorsNotPresent = 1;
+  if (nameV == "" || nameV == null) {
+    errorArr.push("<br>You must enter your name");
+    errorsPresentInDoc = 1;
+  }
+
+  if (emailV == "" || emailV == null) {
+    errorArr.push("<br>You must enter your email address");
+    errorsPresentInDoc = 1;
+  }
+
+  for (var j=0; j<subjectV.length; j++) {
+    if (subjectV[j].checked) {
+      radioErrorsNotPresent = 1;
       break;
     }
   }
-  if (!errorsNotPresent)
-  {
-    errorArr.push("You must select an option for the lifting question!<br>");
+  if (!radioErrorsNotPresent)  {
+    errorArr.push("<br>You must select a subject");
     errorsPresentInDoc = 1;
   }
-  errorsNotPresent = 0;
 
-  for (var j=0; j<cardioRadios.length; j++) {
-    if (cardioRadios[j].checked) {
-      errorsNotPresent = 1;
-      break;
-    }
-  }
-  if (!errorsNotPresent)
-  {
-    errorArr.push("You must select an option for the lifting question!<br>");
-    errorsPresentInDoc = 1;
-  }
-  errorsNotPresent = 0;
-
-  if (problemAnswer == "" || problemAnswer == null)
-  {
-    errorArr.push("You must explain why you avoid your problems!<br>");
+  if (messageV == "" || messageV == null) {
+    errorArr.push("<br>You must enter a message");
     errorsPresentInDoc = 1;
   }
 
   if (!errorsPresentInDoc) {
-    document.getElementById("toReplace").innerHTML="<p>Success!</p>";
+    document.getElementById("contactForm").innerHTML="<p id=\"successId\">The information has been submitted!</p>";
+    document.getElementById("errorSection").innerHTML="";
   }
   else {
-    document.getElementById("errorSection").innerHTML=errorArr.toString() + problemAnswer;
+    document.getElementById("errorSection").innerHTML="Error!\n" + errorArr.toString();
   }
 
   return false;
