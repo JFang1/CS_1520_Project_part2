11fe4b0fd3cf0c8151e96410de4b54a8f6b03661
Fang <jmf133@dlh-m32.labs.pitt.edu>
2016-10-13 01:43:42 -0400

    Mostly done

--- js/javascript.js
+++ js/javascript.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
   // Adds smooth scrolling to links
   // Referenced from http://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_eff_animate_smoothscroll
-  $("a").on('click', function(event) {
+  $('a').on('click', function(event) {
     event.preventDefault(); // prevents default behavior upon link click
     var hash = this.hash; // hash refers to ID'd locations in the page
     $('html, body').animate({
@@ -21,11 +21,12 @@ $(document).ready(function(){
     $windowVar.scroll(function() { // When scrolling occurs
       // A problem here is when you refresh at a hash partway down the page, e.g. [PATH]/index.html#currentProjects, you haven't actually scrolled, so the nav bar doesn't show up
       var scrollPos = $windowVar.scrollTop();
-      var shouldBeFixedBool = scrollPos > originalNavBarPosition; // Have you scrolled down past the nav bar, requiring the nav bar to change position?
+      var shouldBeFixedBool = scrollPos > originalNavBarPosition + 500; // Have you scrolled down past the nav bar + 500, requiring the nav bar to change position?  The 500 makes it so that the float isn't immediate.
       if (shouldBeFixedBool) { // i.e. when you've scrolled down past the nav bar
         nav.css ({
           position: 'fixed', // in relation to the viewing pane
           top: 0, // to keep it at the top of the screen instead of its original location on the screen when you're at the very top
+          left: nav.offset().left,
           width: $windowVar.width(), // Instead of referencing the original width of the nav bar, I'm using the width of the window in case that changes
         });
       }
@@ -37,7 +38,7 @@ $(document).ready(function(){
     });
   });
 
-  $("#contactForm").submit(function(e) {
+  $('#contactForm').submit(function(e) {
     e.preventDefault();
   });
 });
@@ -54,14 +55,14 @@ function validate() {
   var errorArr = [];
 
   // check name
-  if (nameV == "" || nameV == null) {
-    errorArr.push("<br>You must enter your name");
+  if (nameV == '' || nameV == null) {
+    errorArr.push('<br>You must enter your name');
     errorsPresentInDoc = 1;
   }
 
   // check email
-  if (emailV == "" || emailV == null) {
-    errorArr.push("<br>You must enter your email address");
+  if (emailV == '' || emailV == null) {
+    errorArr.push('<br>You must enter your email address');
     errorsPresentInDoc = 1;
   }
 
@@ -73,22 +74,22 @@ function validate() {
     }
   }
   if (!radioErrorsNotPresent)  {
-    errorArr.push("<br>You must select a subject");
+    errorArr.push('<br>You must select a subject');
     errorsPresentInDoc = 1;
   }
 
-  if (messageV == "" || messageV == null) {
-    errorArr.push("<br>You must enter a message");
+  if (messageV == '' || messageV == null) {
+    errorArr.push('<br>You must enter a message');
     errorsPresentInDoc = 1;
   }
 
   // print validation outcome
   if (!errorsPresentInDoc) {
-    document.getElementById("contactForm").innerHTML="<p id=\"successId\">The information has been submitted!</p>";
-    document.getElementById("errorSection").innerHTML="";
+    document.getElementById('contactForm').innerHTML='<p id=\'successId\'>The information has been submitted!</p>';
+    document.getElementById('errorSection').innerHTML='';
   }
   else {
-    document.getElementById("errorSection").innerHTML="Error!\n" + errorArr.toString();
+    document.getElementById('errorSection').innerHTML='Error!\n' + errorArr.toString();
   }
 
   return false;
